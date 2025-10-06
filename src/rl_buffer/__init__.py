f877da5f974539dae607d9c093db944b7ab30f44
windlx <1418043337@qq.com>
2025-10-06 13:20:08 +0800

    导出了 SharedReplayBuffer

--- src/rl_buffer/__init__.py
+++ src/rl_buffer/__init__.py
@@ -1,5 +1,6 @@
 from .base_buffer import BaseBuffer, ResetStrategy
 from .replay_buffer import ReplayBuffer, ReplayBatch
+from .shared_replay_buffer import SharedReplayBuffer
 from .rollout_buffer import RolloutBuffer, RolloutBatch
 from .stats_tracker import StatsTracker
 
@@ -7,6 +8,7 @@ __all__ = [
     "BaseBuffer",
     "ResetStrategy",
     "ReplayBuffer",
+    "SharedReplayBuffer",
     "RolloutBuffer",
     "StatsTracker",
     "ReplayBatch",
