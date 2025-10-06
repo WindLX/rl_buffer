d4934ad700210cad0e0a78e402f6769b2cfcdc5a
windlx <1418043337@qq.com>
2025-10-06 16:25:37 +0800

    删除了并行安全的buffer

--- src/rl_buffer/__init__.py
+++ src/rl_buffer/__init__.py
@@ -1,6 +1,5 @@
 from .base_buffer import BaseBuffer, ResetStrategy
 from .replay_buffer import ReplayBuffer, ReplayBatch
-from .shared_replay_buffer import SharedReplayBuffer
 from .rollout_buffer import RolloutBuffer, RolloutBatch
 from .stats_tracker import StatsTracker
 
@@ -8,7 +7,6 @@ __all__ = [
     "BaseBuffer",
     "ResetStrategy",
     "ReplayBuffer",
-    "SharedReplayBuffer",
     "RolloutBuffer",
     "StatsTracker",
     "ReplayBatch",
